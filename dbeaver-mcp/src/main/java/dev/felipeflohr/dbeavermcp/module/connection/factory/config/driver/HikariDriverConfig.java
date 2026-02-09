490aa32d09193731809f577f13550cbf03e65cff
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2026-02-08 21:21:56 -0300

    #1-Removed comments

--- dbeaver-mcp/src/main/java/dev/felipeflohr/dbeavermcp/module/connection/factory/config/driver/HikariDriverConfig.java
+++ dbeaver-mcp/src/main/java/dev/felipeflohr/dbeavermcp/module/connection/factory/config/driver/HikariDriverConfig.java
@@ -4,14 +4,7 @@ import com.zaxxer.hikari.HikariConfig;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 
-/**
- * Abstraction for Hikari's configuration.
- */
 @NullMarked
 public interface HikariDriverConfig {
-    /**
-     * Returns the config for the implemented driver.
-     * @return {@link HikariConfig} for the driver.
-     */
     HikariConfig getConfig(String jdbcUrl, String username, @Nullable String password);
 }
--- dbeaver-mcp/src/main/java/dev/felipeflohr/dbeavermcp/module/dbeaver/service/DBeaverDataSourceService.java
+++ dbeaver-mcp/src/main/java/dev/felipeflohr/dbeavermcp/module/dbeaver/service/DBeaverDataSourceService.java
@@ -4,14 +4,7 @@ import dev.felipeflohr.dbeavermcp.exception.DBeaverMCPValidationException;
 import dev.felipeflohr.dbeavermcp.module.dbeaver.model.datasources.DBeaverDataSourcesDTO;
 import org.jspecify.annotations.NullMarked;
 
-/**
- * Service for dealing with DBeaver data sources.
- */
 @NullMarked
 public interface DBeaverDataSourceService {
-    /**
-     * @return {@link DBeaverDataSourcesDTO} with data source data.
-     * @throws DBeaverMCPValidationException when user provided data is invalid.
-     */
     DBeaverDataSourcesDTO getDataSources() throws DBeaverMCPValidationException;
 }
